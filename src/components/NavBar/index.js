f177308ed899d784748a779cd960dd1d542bcab8
George Zhihua Yuan <yuangeorge9981@gmail.com>
2020-09-27 21:11:41 -0400

    separated NavBar component from index file

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,19 +1,3 @@
-import React from 'react'
-import styled from 'styled-components'
-
-import colors from 'styles'
-
-const NavBarContainer = styled.div`
-  height: 80px;
-  width: 100%;
-
-  border-bottom-style: solid;
-  border-bottom-width: 1px;
-  border-bottom-color: ${colors.grey};
-`
-
-const NavBar = () => {
-  return <NavBarContainer/>
-}
+import NavBar from './NavBar'
 
 export default NavBar
